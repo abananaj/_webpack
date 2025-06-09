26543dbc9ebde03e531cf8c2674061cc4a89f4f3
abananaj <anna@theatrum.dev>
2025-06-08 19:38:59 -0700

    asset modules configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './development/index.js',
-  output: {
-    path: path.resolve(__dirname, 'production'),
-    filename: 'bundle.js',
-  },
-
+  entry: "./development/index.js",
   plugins: [new HtmlWebpackPlugin({
-    template: "./development/template.html"
+    template: "./development/template.html",
+    scriptLoading: "module",
+    inject: "body"
   })],
-  devServer: {
-    static: path.resolve(__dirname, './production'),
-    liveReload: true,
-    devMiddleware: {
-      publicPath: '/'
-    }
-  },
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
-        exclude: path.resolve(__dirname, './node_modules'),
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif|mp4)/,
+       type: 'asset/resource'
+     }
     ]
-  },
-  ignoreWarnings: [
-    {
-      module: /node_modules/
-    }
-  ]
-
-}
+  }
+};
---
+++ webpack.prod.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.config");
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: "bundle.[contenthash].js",
+        path: path.resolve(__dirname, "production"),
+        assetModuleFilename: "media/[name].[hash][ext][query]"
+    },
+    optimization: {
+        realContentHash: true,
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                extractComments: false,
+                terserOptions: {
+                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                },
+            }),
+        ],
+    }
+});
